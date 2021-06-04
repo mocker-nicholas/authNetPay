48e54bb5e10f10de71d7f2b6ead34a8e329e9430
Nick Orth <nicholas.r.orth@gmail.com>
2021-06-03 19:02:26 -0500

    Start front end validation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,72 @@
-// Make Card Type Display based on card number
+// Personal Info
 const button = document.querySelector('.submit');
+const firstName = document.querySelector('#first');
+const lastName = document.querySelector('#last');
+const company = document.querySelector('#company');
+const address = document.querySelector('#address');
+const city = document.querySelector('#city');
+const state = document.querySelector('#state');
+const zipCode = document.querySelector('#zipcode');
+
+// Payment Info
 const amount = document.querySelector('#amount');
+const invNumber = document.querySelector('#refId');
+const cardNumber = document.querySelector('#cardNumber');
+const expDate = document.querySelector('#expDate');
+const cvv = document.querySelector('#cvv');
+
+// Keys
 const authKey = document.querySelector('#authKey');
 const transKey = document.querySelector('#transKey');
 
 button.addEventListener('click', (e) => {
-  console.log(amount.value)
-  runTrans();
+  validateFields();
+  // runTrans();
 })
 
+// Check Field Values for the user
+const validateFields = () => {
+  const messages = []
+  // First Name
+  if(firstName.value === ""){
+    messages.push('Please enter cardholder first name. Names can only contain letters')
+  } else if (!/^[a-zA-Z]*$/g.test(firstName.value)){
+    messages.push('Names can only contain letters')
+  } 
+  // Last Name
+  if(lastName.value === ""){
+    messages.push('Please enter a cardholder last name. Names can only contain letters')
+  } else if (!/^[a-zA-Z]*$/g.test(firstName.value)){
+    messages.push('Names can only contain letters')
+  }
+  // Billing Address
+  if(address.value === ""){
+    messages.push('Please enter a billing address')
+  }
+
+  // City
+  if(city.value === ""){
+    messages.push('Please enter a city')
+  } else if (!/^[a-zA-Z]*$/g.test(city.value)){
+    messages.push('City can only contain letters')
+  }
+
+  // State
+  if(state.value === ""){
+    messages.push('Please select a state')
+  }
+
+  // Zip
+  if(zipCode.value === ""){
+    messages.push('Please enter a Zip Code')
+  }
+  
+  console.log(messages);
+
+
+}
+
+// Submit Transaction to Auth.net
 const runTrans = () => {
   const transData = {
     "createTransactionRequest": {
@@ -16,31 +74,30 @@ const runTrans = () => {
         "name": authKey.value,
         "transactionKey": transKey.value
       },
-      "refId": "123456",
+      "refId": invNumber.value,
       "transactionRequest": {
         "transactionType": "authCaptureTransaction",
         "amount": amount.value,
         "payment": {
           "creditCard": {
-            "cardNumber": "5424000000000015",
-            "expirationDate": "2025-12",
-            "cardCode": "999"
+            "cardNumber": cardNumber.value,
+            "expirationDate": expDate.value,
+            "cardCode": cvv.value
           }
         },
         "billTo": {
-          "firstName": "Ellen",
-          "lastName": "Johnson",
-          "company": "Souveniropolis",
-          "address": "14 Main Street",
-          "city": "Pecan Springs",
-          "state": "TX",
-          "zip": "44628",
+          "firstName": firstName.value,
+          "lastName": lastName.value,
+          "company": company.value,
+          "address": address.value,
+          "city": city.value,
+          "state": state.value,
+          "zip": zipCode.value,
           "country": "US"
         },
       }
     }
   }
-  console.log(transData)
   fetch('https://apitest.authorize.net/xml/v1/request.api', {
     method: 'POST',
     body: JSON.stringify(transData)
