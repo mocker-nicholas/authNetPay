58238cfb9135969f6de3e32462ec02e9882fdad4
Nick Orth <nicholas.r.orth@gmail.com>
2021-06-08 10:01:25 -0500

    Add webpack/babel dev environment to work computer. Update app.bundle.js

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -1,1 +1,54 @@
-(()=>{var e=document.querySelector(".submit"),t=document.querySelector("#first"),r=document.querySelector("#last"),a=(document.querySelector("#company"),document.querySelector("#address")),n=document.querySelector("#city"),c=document.querySelector("#state"),o=document.querySelector("#zipcode"),d=document.querySelector("#amount"),l=(document.querySelector("#refId"),document.querySelector("#cardNumber"),document.querySelector("#expDate")),u=document.querySelector("#cvv");document.querySelector("#authKey"),document.querySelector("#transKey"),e.addEventListener("click",(function(e){m()}));var m=function(){if(""===t.value){var e=document.querySelector(".input-container-first"),m=document.createElement("p");m.className="error",m.appendChild(document.createTextNode("Please enter cardholder first name")),e.appendChild(m)}else if(!/^[a-zA-Z]*$/g.test(t.value)){var i=document.querySelector(".input-container-first"),s=document.createElement("p");s.className="error",s.appendChild(document.createTextNode("Names can only contain letters")),i.appendChild(s)}if(""===r.value){var p=document.querySelector(".input-container-last"),y=document.createElement("p");y.className="error",y.appendChild(document.createTextNode("Please enter a cardholder last name")),p.appendChild(y)}else if(!/^[a-zA-Z]*$/g.test(t.value)){var v=document.querySelector(".input-container-last"),q=document.createElement("p");q.className="error",q.appendChild(document.createTextNode("Names can only contain letters")),v.appendChild(q)}if(""===a.value){var S=document.querySelector(".input-container-address"),h=document.createElement("p");h.className="error",h.appendChild(document.createTextNode("Please enter a billing address")),S.appendChild(h)}if(""===n.value){var N=document.querySelector(".input-container-city"),C=document.createElement("p");C.className="error",C.appendChild(document.createTextNode("Please enter a city")),N.appendChild(C)}else if(!/^[a-zA-Z]*$/g.test(n.value)){var f=document.querySelector(".input-container-city"),x=document.createElement("p");x.className="error",x.appendChild(document.createTextNode("City can only contain letters")),f.appendChild(x)}if(""===c.value&&messages.push("Please select a state"),""===o.value){var E=document.querySelector(".input-container-zipcode"),T=document.createElement("p");T.className="error",T.appendChild(document.createTextNode("Please enter a zip code")),E.appendChild(T)}else if(!/^[1-9]*$/g.test(o.value)){var z=document.querySelector(".input-container-zipcode"),g=document.createElement("p");g.className="error",g.appendChild(document.createTextNode("Zip code must only contain numbers")),z.appendChild(g)}d.value,d.value,l.value,u.value}})();
+(() => {
+  var e = document.querySelector(".submit"),
+    t = document.querySelector("#first"),
+    r = document.querySelector("#last"),
+    a =
+      (document.querySelector("#company"), document.querySelector("#address")),
+    c = document.querySelector("#city"),
+    n = document.querySelector("#state"),
+    u = document.querySelector("#zipcode"),
+    o = document.querySelector("#amount"),
+    l = document.querySelector("#refId"),
+    m =
+      (document.querySelector("#cardNumber"),
+      document.querySelector("#expDate")),
+    d = document.querySelector("#cvv");
+  document.querySelector("#authKey"),
+    document.querySelector("#transKey"),
+    e.addEventListener("click", function (e) {
+      i();
+    });
+  var i = function () {
+      "" === t.value
+        ? s("#first-item", "Enter a first name")
+        : /^[a-zA-Z]*$/g.test(t.value) ||
+          s("#first-item", "Names can only contain letters"),
+        "" === r.value
+          ? s("#last-item", "Enter a last name")
+          : /^[a-zA-Z]*$/g.test(t.value) ||
+            s("#last-item", "Names can only contain letters"),
+        "" === a.value && s("#address-item", "Enter a biling address"),
+        "" === c.value
+          ? s("#city-item", "Enter a city")
+          : /^[a-zA-Z]*$/g.test(c.value) ||
+            s("#city-item", "City can only contain letters"),
+        "" === n.value && s("#state-item", "Select a state"),
+        "" === u.value
+          ? s("#zipcode-item", "Enter a zipcode")
+          : /^[1-9]*$/g.test(u.value) ||
+            s("#zipcode-item", "Zipcode can only contain numbers"),
+        "" === l.value && s("#refId-item", "Enter an Invoice number"),
+        "" === o.value && s("#amount-item", "Enter an amount"),
+        "" === o.value && s("#cardNumber-item", "Enter card number"),
+        "" === m.value && s("#expDate-item", "Enter card exp. date"),
+        "" === d.value && s("#cvv-item", "Enter card cvv");
+    },
+    s = function (e, t) {
+      var r = document.querySelector(e),
+        a = document.createElement("p");
+      (a.className = "error"),
+        a.appendChild(document.createTextNode(t)),
+        r.appendChild(a);
+    };
+  Testtesttesttesttest;
+})();
--- src/app.js
+++ src/app.js
@@ -1,131 +1,131 @@
 // Personal Info
-const button = document.querySelector('.submit');
-const firstName = document.querySelector('#first');
-const lastName = document.querySelector('#last');
-const company = document.querySelector('#company');
-const address = document.querySelector('#address');
-const city = document.querySelector('#city');
-const state = document.querySelector('#state');
-const zipCode = document.querySelector('#zipcode');
+const button = document.querySelector(".submit");
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const company = document.querySelector("#company");
+const address = document.querySelector("#address");
+const city = document.querySelector("#city");
+const state = document.querySelector("#state");
+const zipCode = document.querySelector("#zipcode");
 
 // Payment Info
-const amount = document.querySelector('#amount');
-const invNumber = document.querySelector('#refId');
-const cardNumber = document.querySelector('#cardNumber');
-const expDate = document.querySelector('#expDate');
-const cvv = document.querySelector('#cvv');
+const amount = document.querySelector("#amount");
+const invNumber = document.querySelector("#refId");
+const cardNumber = document.querySelector("#cardNumber");
+const expDate = document.querySelector("#expDate");
+const cvv = document.querySelector("#cvv");
 
 // Keys
-const authKey = document.querySelector('#authKey');
-const transKey = document.querySelector('#transKey');
+const authKey = document.querySelector("#authKey");
+const transKey = document.querySelector("#transKey");
 
-button.addEventListener('click', (e) => {
+button.addEventListener("click", (e) => {
   validateFields();
   // runTrans();
   // clearError();
-})
+});
 
 // Check Field Values for the user
 const validateFields = () => {
   // First Name
-  if(firstName.value === ""){
-    showError('#first-item', 'Enter a first name')
-  } else if (!/^[a-zA-Z]*$/g.test(firstName.value)){
-    showError('#first-item', 'Names can only contain letters')
-  } 
+  if (firstName.value === "") {
+    showError("#first-item", "Enter a first name");
+  } else if (!/^[a-zA-Z]*$/g.test(firstName.value)) {
+    showError("#first-item", "Names can only contain letters");
+  }
   // Last Name
-  if(lastName.value === ""){
-    showError('#last-item', 'Enter a last name')
-  } else if (!/^[a-zA-Z]*$/g.test(firstName.value)){
-    showError('#last-item', 'Names can only contain letters')
+  if (lastName.value === "") {
+    showError("#last-item", "Enter a last name");
+  } else if (!/^[a-zA-Z]*$/g.test(firstName.value)) {
+    showError("#last-item", "Names can only contain letters");
   }
   // Billing Address
-  if(address.value === ""){
-    showError('#address-item', 'Enter a biling address')
+  if (address.value === "") {
+    showError("#address-item", "Enter a biling address");
   }
   // City
-  if(city.value === ""){
-    showError('#city-item', 'Enter a city')
-  } else if (!/^[a-zA-Z]*$/g.test(city.value)){
-    showError('#city-item', 'City can only contain letters')
+  if (city.value === "") {
+    showError("#city-item", "Enter a city");
+  } else if (!/^[a-zA-Z]*$/g.test(city.value)) {
+    showError("#city-item", "City can only contain letters");
   }
   // State
-  if(state.value === ""){
-    showError('#state-item', 'Select a state')
+  if (state.value === "") {
+    showError("#state-item", "Select a state");
   }
   // Zip
-  if(zipCode.value === ""){
-    showError('#zipcode-item', 'Enter a zipcode')
-  } else if (!/^[1-9]*$/g.test(zipCode.value)){
-    showError('#zipcode-item', 'Zipcode can only contain numbers')
+  if (zipCode.value === "") {
+    showError("#zipcode-item", "Enter a zipcode");
+  } else if (!/^[1-9]*$/g.test(zipCode.value)) {
+    showError("#zipcode-item", "Zipcode can only contain numbers");
   }
   // Invoice Number
-  if(invNumber.value === ""){
-    showError('#refId-item', 'Enter an Invoice number')
+  if (invNumber.value === "") {
+    showError("#refId-item", "Enter an Invoice number");
   }
   // Amount
-  if(amount.value === ""){
-    showError('#amount-item', 'Enter an amount')
+  if (amount.value === "") {
+    showError("#amount-item", "Enter an amount");
   }
   // Card Number
-  if(amount.value === ""){
-    showError('#cardNumber-item', 'Enter card number')
+  if (amount.value === "") {
+    showError("#cardNumber-item", "Enter card number");
   }
   // Exp Date
-  if(expDate.value === ""){
-    showError('#expDate-item', 'Enter card exp. date')
+  if (expDate.value === "") {
+    showError("#expDate-item", "Enter card exp. date");
   }
   // Cvv
-  if(cvv.value === ""){
-    showError('#cvv-item', 'Enter card cvv')
+  if (cvv.value === "") {
+    showError("#cvv-item", "Enter card cvv");
   }
-}
+};
 
 // Validation Helper Function
 const showError = (flexItemId, errorMessage) => {
   const inputDiv = document.querySelector(flexItemId);
-  const errorMessageEl = document.createElement('p')
-  errorMessageEl.className = 'error'
-  errorMessageEl.appendChild(document.createTextNode(errorMessage))
+  const errorMessageEl = document.createElement("p");
+  errorMessageEl.className = "error";
+  errorMessageEl.appendChild(document.createTextNode(errorMessage));
   inputDiv.appendChild(errorMessageEl);
-}
+};
 
 // Submit Transaction to Auth.net
 const runTrans = () => {
   const transData = {
-    "createTransactionRequest": {
-      "merchantAuthentication": {
-        "name": authKey.value,
-        "transactionKey": transKey.value
+    createTransactionRequest: {
+      merchantAuthentication: {
+        name: authKey.value,
+        transactionKey: transKey.value,
       },
-      "refId": invNumber.value,
-      "transactionRequest": {
-        "transactionType": "authCaptureTransaction",
-        "amount": amount.value,
-        "payment": {
-          "creditCard": {
-            "cardNumber": cardNumber.value,
-            "expirationDate": expDate.value,
-            "cardCode": cvv.value
-          }
+      refId: invNumber.value,
+      transactionRequest: {
+        transactionType: "authCaptureTransaction",
+        amount: amount.value,
+        payment: {
+          creditCard: {
+            cardNumber: cardNumber.value,
+            expirationDate: expDate.value,
+            cardCode: cvv.value,
+          },
         },
-        "billTo": {
-          "firstName": firstName.value,
-          "lastName": lastName.value,
-          "company": company.value,
-          "address": address.value,
-          "city": city.value,
-          "state": state.value,
-          "zip": zipCode.value,
-          "country": "US"
+        billTo: {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          company: company.value,
+          address: address.value,
+          city: city.value,
+          state: state.value,
+          zip: zipCode.value,
+          country: "US",
         },
-      }
-    }
-  }
-  fetch('https://apitest.authorize.net/xml/v1/request.api', {
-    method: 'POST',
-    body: JSON.stringify(transData)
+      },
+    },
+  };
+  fetch("https://apitest.authorize.net/xml/v1/request.api", {
+    method: "POST",
+    body: JSON.stringify(transData),
   })
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
