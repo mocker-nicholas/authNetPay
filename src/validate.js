1ae0c8b1f24a7cf0f0dc4e381f8237102510d34d
Nick Orth <nicholas.r.orth@gmail.com>
2021-06-19 15:57:50 -0500

    Add validate module

---
+++ src/validate.js
@@ -0,0 +1,11 @@
+const validate = {
+  showError: function(flexItemId, errorMessage) {
+    const inputDiv = document.querySelector(flexItemId);
+    const errorMessageEl = document.createElement("p");
+    errorMessageEl.className = "error";
+    errorMessageEl.appendChild(document.createTextNode(errorMessage));
+    inputDiv.appendChild(errorMessageEl);
+  }
+}
+
+export default validate
